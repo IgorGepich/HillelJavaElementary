ebb784b2d068bbaab9908427939aebcd58ca0ea3
Igor <is.gepich@gmail.com>
2019-09-11 22:53:25 +0300

    ALesson01 final implementation

--- src/ua/hillel/lessons/lesson01/MainLesson01.java
+++ src/ua/hillel/lessons/lesson01/MainLesson01.java
@@ -1,12 +1,8 @@
 package ua.hillel.lessons.lesson01;
 
-import ua.hillel.lessons.lesson01.Lesson01;
-
 public class MainLesson01 {
 	public static void main(String[] args) {
 
-/* LESSON 01
-* */
 		Lesson01 lesson01 = new Lesson01();
 		lesson01.minMax();
 		lesson01.centuryYear();
