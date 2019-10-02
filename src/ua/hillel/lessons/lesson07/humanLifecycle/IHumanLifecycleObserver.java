0ff430fc4c4527698c2bf08eda0f72460f6ad90c
Igor <is.gepich@gmail.com>
2019-10-03 00:01:04 +0300

    add lesson07 Lifecycle implementation

--- src/ua/hillel/lessons/lesson07/humanLifecycle/IHumanLifecycleObserver.java
+++ src/ua/hillel/lessons/lesson07/humanLifecycle/IHumanLifecycleObserver.java
@@ -5,32 +5,179 @@ import java.util.Date;
  *  The interface defines an observer over {@link HumanLifecycle}
  */
 public interface IHumanLifecycleObserver {
+
 	/**
 	 * Handles the first event in a human life.
-	 * @param params The stage parameters.
+	 * @param bornParams The stage parameters.
 	 */
-	// on - на событии, реакция на определенное действие, далее описание этого события
-	void onHumanWasBorn(HumanBornParams params);
-	void onHumanKindergarten(KindergartenParams params);
+
+	void onHumanWasBorn(HumanBornParams bornParams);
+
+	/**
+	 * Handles the second event in a human life.
+	 * @param kindergartenParams The stage parameters.
+	 */
+
+	void onHumanKindergarten(KindergartenParams kindergartenParams);
+
+	/**
+	 * Handles the third event in a human life.
+	 * @param schoolParams The stage parameters.
+	 */
+
+	void onHumanSchoolYears(SchoolParams schoolParams);
+
+	/**
+	 * Handles the fourth event in a human life.
+	 * @param instituteParams The stage parameters.
+	 */
+	void onHumanInstitute(InstituteParams instituteParams);
+
+	/**
+	 * Handles the fifth event in a human life.
+	 * @param weddingParams The stage parameters.
+	 */
+
+	void onWedding(WeddingParams weddingParams);
+
+	/**
+	 * Handles the sixth event in a human life.
+	 * @param childbirthParams The stage parameters.
+	 */
+
+	void onChildbirth(ChildbirthParams childbirthParams);
+
+	/**
+	 * Handles the seventh event in a human life.
+	 * @param pensionParams The stage parameters.
+	 */
+
+	void onPension(PensionParams pensionParams);
+
+	/**
+	 * Handles the eighth event in a human life.
+	 * @param gameOverParams stage parameters.
+	 */
+
+	void onGameOver(GameOverParams gameOverParams);
+
 	/**
 	 * Attributes of the born stage of a human
 	 */
+
 	class HumanBornParams{
+
 	/**
-	* @param name the name of the human.
-	* @param birthdate the date when the human was born.
-	* @param weight the weight of the child in kilograms.
-	* @param gender the tolerant gender of the child.
+	* HumanName the name of the human.
+	* Birthdate the date when the human was born.
+	* Weight the weight of the child in kilograms.
+	* Gender the tolerant gender of the child.
 	**/
-		public String name;
-		public Date birthdate;
-		public float weight;
-		public String gender;
+		static String humanName;
+		Date birthdate;
+		float humanWeight;
+		String gender;
 	}
+
+	/**
+	 * Attributes of the kindergarten stage of a human.
+	 */
 
 	class KindergartenParams{
-		public int period;
+
+	/**
+	* Age the age of the human.
+	* Period the period that the child was in kindergarten.
+	*/
+
+		String name = HumanBornParams.humanName;
+		int humanAge;
+		int periodOfVisitGarden;
+		int frequencyOfVisits;
 	}
 
+	/**
+	 * Attributes of the school stage of a human.
+	 */
 
+	class SchoolParams{
+
+		/**
+		 * SchoolNumber
+		 */
+
+		int schoolNumber;
+		int periodOfStudyingInSchool;
+
+	}
+
+	/**
+	 * Attributes of the institute stage of a human.
+	 */
+
+	class InstituteParams{
+		/**
+		 * InstituteName the name of the institute where the human studied in.
+		 * Faculty
+		 * PeriodOfStudyingInInstitute
+		 */
+
+		String instituteName;
+		String faculty;
+		int periodOfStudyingInInstitute;
+	}
+
+	/**
+	 * Attributes of the family life stage of a human.
+	 */
+
+	class WeddingParams{
+
+		/**
+		 * WifesName the name of the human wife.
+		 * WifesAge human wifes name.
+		 */
+
+		String wifesName;
+		int wifesAge;
+	}
+
+	/**
+	 * Attributes of childbirth stage of a human.
+	 */
+
+	class ChildbirthParams {
+
+		/**
+		 * ChildName child name
+		 * ChildWeight child weight
+		 * ChildGender child gender
+		 */
+
+		String childName;
+		float childWeight;
+		String childGender;
+	}
+
+	/**
+	 * Attributes of the pension stage of a human.
+	 */
+
+	class PensionParams{
+		/**
+		 * The country where the human lives in after retirement.
+		 */
+		String country;
+	}
+
+	/**
+	 * Attributes of the end of life of the human.
+	 */
+
+	class GameOverParams{
+		/**
+		 * Disease cause of death
+		 */
+		String disease;
+	}
 }
--- src/ua/hillel/lessons/lesson07/humanLifecycle/MainLesson07Lifecycle.java
+++ src/ua/hillel/lessons/lesson07/humanLifecycle/MainLesson07Lifecycle.java
@@ -5,27 +5,69 @@ public class MainLesson07Lifecycle {
 	 * The main entry point
 	 */
 	public static void main(String[] args) {
-		IHumanLifecycleObserver observer = new LoggingHumanLifecycleObserver();
+		IHumanLifecycleObserver observer = new IHumanLifecycleObserver() {
+			@Override
+			public void onHumanWasBorn(HumanBornParams bornParams) {
+				System.out.println(String.format("Child name %s \nwas born %s \nwith wight %.2f \ngender %s",
+						HumanBornParams.humanName, bornParams.birthdate, bornParams.humanWeight, bornParams.gender));
+			}
+
+			@Override
+			public void onHumanKindergarten(KindergartenParams kindergartenParams) {
+				System.out.println(String.format("At the age of %d %s went to kindergarten for %d years " +
+								"and visited the garden %s times a week",
+						kindergartenParams.humanAge, kindergartenParams.name, kindergartenParams.periodOfVisitGarden,
+						kindergartenParams.frequencyOfVisits));
+			}
+
+			@Override
+			public void onHumanSchoolYears(SchoolParams schoolParams) {
+				System.out.println(String.format("Studied at school number %d for %d years",
+						schoolParams.schoolNumber, schoolParams.periodOfStudyingInSchool));
+			}
+
+			@Override
+			public void onHumanInstitute(InstituteParams instituteParams) {
+				System.out.println(String.format("Throughout %d years studied at %s on %s faculty.",
+						instituteParams.periodOfStudyingInInstitute, instituteParams.instituteName, instituteParams.faculty));
+			}
+
+			@Override
+			public void onWedding(WeddingParams weddingParams) {
+				System.out.println(String.format("Wife: \nage %s wifes name: %s",
+						weddingParams.wifesAge, weddingParams.wifesName));
+			}
+
+			@Override
+			public void onChildbirth(ChildbirthParams childbirthParams) {
+				System.out.println(String.format("Child: \nname %s \ngender: %s \nweight: %.2f kilograms.",
+						childbirthParams.childName, childbirthParams.childGender,childbirthParams.childWeight));
+			}
+
+			@Override
+			public void onPension(PensionParams pensionParams) {
+				System.out.println(String.format("Retired moved to %s", pensionParams.country));
+			}
+
+			@Override
+			public void onGameOver(GameOverParams gameOverParams) {
+				System.out.println(String.format("Died of a %s", gameOverParams.disease));
+			}
+
+		};
 
 		HumanLifecycle lifecycle = new HumanLifecycle(observer);
 
 		// begin the life
 		lifecycle.startLife();
-	}
-	private static class LoggingHumanLifecycleObserver implements IHumanLifecycleObserver{
-
-		@Override
-		public void onHumanWasBorn(HumanBornParams params) {
-			System.out.println(String.format("Child name %s \nwas born %s \nwith wight %.2f \ngender %s",
-					params.name, params.birthdate, params.weight, params.gender));
-		}
-
-		@Override
-		public void onHumanKindergarten(KindergartenParams params) {
-			System.out.println(String.format("Child name /s",
-					params.));
-		}
-
+		lifecycle.kindergarten();
+		lifecycle.school();
+		lifecycle.institute();
+		lifecycle.wedding();
+		lifecycle.childbirth();
+		lifecycle.pension();
+		lifecycle.gameOver();
+		//end of life
 
 	}
 }
