16eafce90d87d31abcd6be8aa3c1e6b81817c3f8
Igor <is.gepich@gmail.com>
2019-11-30 20:43:55 +0200

    Lesson02 homeTask final

--- src/ua/hillel/lessons/lesson02/SortAndFind.java
+++ src/ua/hillel/lessons/lesson02/SortAndFind.java
@@ -4,17 +4,17 @@ import java.util.Arrays;
 
 public class SortAndFind {
 
-	public Student[] studentsArray = new Student[10];
-	public StudentFactory studentFactory = new StudentFactory();
+	private Student[] studentsArray = new Student[10];
+	private StudentFactory studentFactory = new StudentFactory();
 
-	public Student[] fillArray() {
+	Student[] fillArray() {
 		for (int i = 0; i < studentsArray.length; i++) {
 			studentsArray[i] = studentFactory.studentCreate();
 			System.out.println(studentsArray[i].getFullStudentName());
 		}
 		return studentsArray;
 	}
-	public Student[] sortStudentsByID(){
+	Student[] sortStudentsByID(){
 			for (int i = 0; i < studentsArray.length; i++){
 			for (int j = 0; j < studentsArray.length; j++){
 				if (studentsArray[i].studentId < studentsArray[j].studentId){
@@ -29,7 +29,7 @@ public class SortAndFind {
 	/**
 	Realization of Find and FindStudentByName methods can find Student by name, or by partial name match.
 	 */
-	public void find(){
+	void find(){
 	Student students = findStudentByName(studentsArray, "Igor");
 		for(int i = 0; i < studentsArray.length; i++) {
 			if (students == null) {
@@ -41,23 +41,23 @@ public class SortAndFind {
 			}
 		}
 	}
-	public Student findStudentByName(Student[] studentsArray, String nameToFind) {
+	private Student findStudentByName(Student[] studentsArray, String nameToFind) {
 		Student foundStudents = null;
-		for(int i = 0; i < studentsArray.length; i++) {
-			if (studentsArray[i].name.contains(nameToFind)) {
-				foundStudents = studentsArray[i];
+		for (Student student : studentsArray) {
+			if (student.name.contains(nameToFind)) {
+				foundStudents = student;
 				break;
 			}
 		}
 		return foundStudents;
 	}
-	public void sortByName(){
+	void sortByName(){
 		Arrays.sort(studentsArray);
 		System.out.println("Sort array by Name field" + Arrays.asList(studentsArray));
 	}
 	public void print(){
-		for(int i = 0; i < studentsArray.length; i++){
-			System.out.println(studentsArray[i].name + " " + studentsArray[i].studentId);
+		for (Student student : studentsArray) {
+			System.out.println(student.name + " " + student.studentId);
 		}
 	}
 }
